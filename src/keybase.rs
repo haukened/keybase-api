3a3a6b62c0bf38f557c21e63291f9de9ff0cfa8c
David Haukeness <david@hauken.us>
2020-01-01 14:49:05 -0700

    initial commit on keybase package

---
+++ src/keybase.rs
@@ -0,0 +1,60 @@
+use crate::keybase_cmd;
+use super::StatusResponse;
+use std::{
+    fmt,
+    path::PathBuf,
+    thread::JoinHandle,
+};
+
+pub mod keybase_error {
+    error_chain! {
+        foreign_links {
+            Parsing(::serde_json::error::Error);
+            IOErr(::std::io::Error);
+            UTF8Err(std::string::FromUtf8Error);
+        }        
+    }
+}
+
+use super::keybase_error::*;
+
+pub struct Keybase {
+    pub username: String,
+    paperkey: String,
+    pub keybase_path: PathBuf,
+    pub status: StatusResponse,
+    pub listen_threads: Vec<JoinHandle<()>>,
+}
+
+impl fmt::Debug for Keybase {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!( f, "Keybase {{ username: {}, status: {:?}, listen_threads: {} }}",
+            self.username,
+            self.status,
+            self.listen_threads.len()
+        )
+    }
+}
+
+impl Keybase {
+    pub fn new<S>(username: S, paperkey: S, opt_path: Option<PathBuf>) -> Result<Keybase> 
+    where 
+        S: Into<String>,
+    {
+        let username: String = username.into();
+        let paperkey: String = paperkey.into();
+        // use use specified keybase path OR
+        let keybase_path: PathBuf = opt_path.unwrap_or(
+            // use `which` to find the keybase binary OR
+            keybase_cmd::find_keybase()?
+            );
+        let keybase_status: StatusResponse = keybase_cmd::call_status(&keybase_path)?;
+        Ok(Keybase {
+            username,
+            paperkey,
+            keybase_path,
+            status: keybase_status,
+            listen_threads: vec![],
+        })
+    }
+}
